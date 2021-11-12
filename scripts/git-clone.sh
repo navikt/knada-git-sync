3fc8e6144241d6344a0dbb3ed086bd4fa46065e0
Kyrre Havik <kyrremann@gmail.com>
2021-11-12 14:59:28 +0100

    fix: k8s emptyStorage er alltid tom
    klarte heller ikke se at de rm-kommandoene faktisk gjorde noe

--- scripts/git-clone.sh
+++ scripts/git-clone.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+
 REPO=$1
 REF=$2
 DIR=$3
@@ -8,11 +9,5 @@ export GITHUB_REPOSITORY="${REPO}"
 
 TOKEN=$(/bin/bash /github-app-token-generator/get-installation-access-token.sh "$(cat /keys/PRIVATE_KEY)" "$(cat /keys/APP_ID)") && \
 TOKEN="${TOKEN#::set-output name=token::}"
-
-# ensure the git directory is empty, so we can safely clone
-if [ -d "$DIR" ]; then
-    rm -rf "$( find "$DIR" -mindepth 1 )"
-    rm -rf "${DIR:?}/*"
-fi
 
 git clone "https://x-access-token:$TOKEN@github.com/$REPO" -b "$REF" "$DIR"
