1ca26368f31d11753ad1ff116aba37f2a730148c
Kyrre Havik <kyrremann@gmail.com>
2021-11-12 10:20:21 +0100

    fix: kjørt shellcheck og opprydding

--- scripts/git-clone.sh
+++ scripts/git-clone.sh
@@ -4,15 +4,14 @@ REPO=$1
 REF=$2
 DIR=$3
 
-export GITHUB_REPOSITORY=${REPO}
+export GITHUB_REPOSITORY="${REPO}"
 
-TOKEN=$(/bin/bash /github-app-token-generator/get-installation-access-token.sh "$(cat /keys/PRIVATE_KEY)" $(cat /keys/APP_ID)) && \
+TOKEN=$(/bin/bash /github-app-token-generator/get-installation-access-token.sh "$(cat /keys/PRIVATE_KEY)" "$(cat /keys/APP_ID)") && \
 TOKEN="${TOKEN#::set-output name=token::}"
 
 # ensure the git directory is empty, so we can safely clone
 if [ -d "$DIR" ]; then
-  rm -rf $( find $DIR -mindepth 1 )
-  rm -rf $DIR/*
+  rm -rf "$DIR"
 fi
 
-git clone https://x-access-token:$TOKEN@github.com/$REPO -b $REF $DIR
+git clone "https://x-access-token:$TOKEN@github.com/$REPO" -b "$REF" "$DIR"
--- scripts/git-sync.sh
+++ scripts/git-sync.sh
@@ -5,25 +5,26 @@ REF=$2
 DIR=$3
 SYNC_TIME=$4
 
-export GITHUB_REPOSITORY=${REPO}
+export GITHUB_REPOSITORY="${REPO}"
 
-TOKEN=$(/bin/bash /github-app-token-generator/get-installation-access-token.sh "$(cat /keys/PRIVATE_KEY)" $(cat /keys/APP_ID)) && \
+TOKEN=$(/bin/bash /github-app-token-generator/get-installation-access-token.sh "$(cat /keys/PRIVATE_KEY)" "$(cat /keys/APP_ID)") && \
 TOKEN="${TOKEN#::set-output name=token::}"
 
 if [ -d "$DIR" ]; then
-  rm -rf $( find $DIR -mindepth 1 )
-  rm -rf $DIR/*
+  rm -rf "$DIR"
 fi
-git clone https://x-access-token:$TOKEN@github.com/$REPO -b $REF $DIR
+echo "Cloning next with $REPO $REF $DIR"
+echo "git clone -v https://x-access-token:$TOKEN@github.com/$REPO $DIR"
+git clone -v "https://x-access-token:$TOKEN@github.com/$REPO" "$DIR"
 
 # to break the infinite loop when we receive SIGTERM
-trap "exit 0" SIGTERM
+trap "exit 0" TERM
 
-cd $DIR
+cd "$DIR"
 while true; do
-  git fetch origin $REF;
-  git reset --hard origin/$REF;
+  git fetch origin "$REF";
+  git reset --hard "origin/$REF";
   git clean -fd;
   date;
-  sleep $SYNC_TIME;
+  sleep "$SYNC_TIME";
 done
