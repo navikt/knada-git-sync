aa8066b8014437fa6430e1426c84667c3406d4f6
Erik Vattekar <erik.vattekar@nav.no>
2023-10-19 14:43:55 +0200

    allow unauthenticated clone in git-clone for public repos

--- scripts/git-clone.sh
+++ scripts/git-clone.sh
@@ -13,20 +13,26 @@ else
     DIR=$3
 fi
 
-export GITHUB_REPOSITORY="${REPO}"
+if [ -e /keys/PRIVATE_KEY ]
+then
+    export GITHUB_REPOSITORY="${REPO}"
 
-# get-installation-access-token.sh stores the fetched access token in a file referenced by the env GITHUB_OUTPUT
-# so we must create this file
-touch /tmp/token
-export GITHUB_OUTPUT=/tmp/token
+    # get-installation-access-token.sh stores the fetched access token in a file referenced by the env GITHUB_OUTPUT
+    # so we must create this file
+    touch /tmp/token
+    export GITHUB_OUTPUT=/tmp/token
 
-/github-app-token-generator/get-installation-access-token.sh "$(cat /keys/PRIVATE_KEY)" "$(cat /keys/APP_ID)"
-TOKEN=$(tail -1 /tmp/token)
-TOKEN=${TOKEN#"token="}
+    /github-app-token-generator/get-installation-access-token.sh "$(cat /keys/PRIVATE_KEY)" "$(cat /keys/APP_ID)"
+    TOKEN=$(tail -1 /tmp/token)
+    TOKEN=${TOKEN#"token="}
+    CREDS="x-access-token:$TOKEN"
+else
+    CREDS=""
+fi
 
 for i in {1..3}
 do 
-    git clone --quiet "https://x-access-token:$TOKEN@github.com/$REPO" -b "$REF" "$DIR" && break || echo "retrying in 5 seconds..."; sleep 5
+    git clone --quiet "https://$CREDS@github.com/$REPO" -b "$REF" "$DIR" && break || echo "retrying in 5 seconds..."; sleep 5
 done
 
 sleep 2
