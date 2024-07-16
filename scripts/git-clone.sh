79c61876a265e7668479d79dd10ff377b34ce192
Erik Vattekar <erik.vattekar@nav.no>
2024-07-16 14:47:41 +0200

    retry logic for access token fetch and git clone

--- scripts/git-clone.sh
+++ scripts/git-clone.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -e
 
 ARGLEN=$#
 if [ $ARGLEN -lt 3 ]
@@ -22,9 +21,17 @@ then
     touch /tmp/token
     export GITHUB_OUTPUT=/tmp/token
 
-    for i in {1..3}
+    attempt_num=1
+    while [ $attempt_num -le 3 ];
     do
-        /github-app-token-generator/get-installation-access-token.sh "$(cat /keys/PRIVATE_KEY)" "$(cat /keys/APP_ID)" && break || echo "retrying fetching access token in 5 seconds..."; sleep 5
+        /github-app-token-generator/get-installation-access-token.sh "$(cat /keys/PRIVATE_KEY)" "$(cat /keys/APP_ID)"
+        if [ $? -eq 0 ]; then
+          break
+        else
+          attempt_num=$(( attempt_num + 1 ))
+          echo "retrying fetching access token in 5 seconds"
+          sleep 5
+        fi
     done
     TOKEN=$(tail -1 /tmp/token)
     TOKEN=${TOKEN#"token="}
@@ -33,9 +40,17 @@ else
     CREDS=""
 fi
 
-for i in {1..3}
-do 
-    git clone --depth=1 --quiet "https://$CREDS@github.com/$REPO" -b "$REF" "$DIR" && break || echo "retrying in 5 seconds..."; sleep 5
+attempt_num=1
+while [ $attempt_num -le 3 ];
+do
+    git clone --depth=1 --quiet "https://$CREDS@github.com/$REPO" -b "$REF" "$DIR"
+    if [ $? -eq 0 ]; then
+        break
+    else 
+        attempt_num=$(( attempt_num + 1 ))
+        echo "retrying in 5 seconds..."
+        sleep 5
+    fi
 done
 
 sleep 2
